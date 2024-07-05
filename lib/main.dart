882f0fab3f5f0bfbbc205ae5af9b9bf444773168
HaamzyTech <haamzy.hm@outlook.com>
2024-07-05 22:31:35 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:vercel/layout/desktop_scaffold.dart';
+import 'package:vercel/layout/mobile_scaffold.dart';
+import 'package:vercel/layout/responsive_layout.dart';
+import 'package:vercel/layout/tablet_scaffold.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HaamzyTech',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ResponsiveLayout(
+        mobileScaffold: MobileScaffold(),
+        tabletScaffold: TabletScaffold(),
+        desktopScaffold: DesktopScaffold(),
+      ),
+    );
+  }
+}
